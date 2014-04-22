fc07beb60a69c6fa6c7bc88dab94ec13d2eeab00
unknown <nicole.oonk@hotmail.com>
2014-04-22 16:16:12 +0200

    scr added

---
+++ src/main/java/nl/utwente/di/Quoter.java
@@ -0,0 +1,31 @@
+package nl.utwente.di;
+
+public class Quoter {
+
+	public double getBookPrice(String i){
+		
+		if(i.equals("1")){
+			return 10.0;
+		}
+		
+		if(i.equals("2")){
+			return 45.0;
+		}
+		if(i.equals("3")){
+			return 20.0;
+		}
+		if(i.equals("4")){
+			return 35.0;
+		}
+		if(i.equals("5")){
+			return 50.0;
+		}
+		
+		else{
+			return 0.0;
+		}
+		
+		
+		
+	}
+}
